9b5a032327ba0805afaf37b348ac9541ceb81b2e
dap2468 <dap2468@gmail.com>
2023-03-12 22:16:07 -0500

    final commit

--- embeded/src/main.cpp
+++ embeded/src/main.cpp
@@ -2,22 +2,23 @@
 #include <Wifi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include "env.h" // remember to add this file to the .gitignore
+#include "env.h" 
+
 
 #define Fan 22
 #define Light 23
 
 
-
 void setup() {
-  pinMode(Light, OUTPUT);
-  pinMode(Fan, OUTPUT);
-  
+
   Serial.begin(9600);
-	
+
+	pinMode(Fan,OUTPUT);
+  pinMode(Light,OUTPUT);
+
 	// WiFi_SSID and WIFI_PASS should be stored in the env.h
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-
+  Serial.println("");
 	// Connect to wifi
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
@@ -32,80 +33,67 @@ void setup() {
 void loop() {
   //Check WiFi connection status
   if(WiFi.status()== WL_CONNECTED){
-
+    Serial.println("");
+    Serial.println("");
     HTTPClient http;
   
-
-  float temp = random(21.0,33.0);
     // Establish a connection to the server
+    float test=random(21.0,33.0);
     http.begin(endpoint);
-    
-    // Specify content-type header
-   
+    http.addHeader("Content-type", "application/json");
 
-      http.addHeader("Content-Type", "application/json");
-      http.addHeader("Content-length", "23");
-      
-
-      // Serialise JSON object into a string to be sent to the API
-      StaticJsonDocument<1024> doc;
+    StaticJsonDocument<23> doc;
       String httpRequestData;
 
-      
-
-
-      doc["temperature"] =temp;
-
+      // Serialise JSON object into a string to be sent to the API
+      doc["temperature"] = test;
+  
+      // convert JSON document, doc, to string and copies it into httpRequestData
       serializeJson(doc, httpRequestData);
 
 
-      // Send HTTP PUT request
-      int httpResponseCode = http.PUT(httpRequestData);
-      String http_response;
+    // Send HTTP PUT request
+    int httpResponseCode = http.PUT(httpRequestData);
+    String http_response;
 
-      // check reuslt of POST request. negative response code means server wasn't reached
-     
-   
+    // check reuslt of PUT request. negative response code means server wasn't reached
+    if (httpResponseCode>0) {
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
 
+      Serial.print("HTTP Response from server: ");
       http_response = http.getString();
       Serial.println(http_response);
-      
-      // Free resources
-      http.end();
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
 
-
-
+    http.end();    
 
 
 
     http.begin(endpoint);
-    
+    httpResponseCode = http.GET();
 
-    int httpResponseCode2 = http.GET();
+    Serial.println("");
+    Serial.println("");
 
-    if(httpResponseCode2>0){
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode2);
+    if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
 
-      Serial.print("Response from server: ");
-      http_response = http.getString();
-      
-
+        Serial.print("Response from server: ");
+        http_response = http.getString();
+        Serial.println(http_response);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
     }
-    else {
-      Serial.print("Error Code: ");
-      Serial.println(httpResponseCode2);
-    }
-
-    
-
-    // Stream& input;
-
-   
-
-    StaticJsonDocument<192> doc2;
+ 
+    StaticJsonDocument<1024> doc2;
 
     DeserializationError error = deserializeJson(doc2, http_response);
 
@@ -114,30 +102,15 @@ void loop() {
       Serial.println(error.c_str());
       return;
     }
-
-    const char* id = doc["_id"]; // "640e3d68bf11b4131596c840"
-    float temperature = doc["temperature"]; // 3.2
-    bool light = doc["light"]; // true
-    bool fan = doc["fan"]; // false
-    const char* poke = doc["poke"]; // "mon"
-
-    Serial.println("");
-
-    Serial.print("Fan: ");
-    Serial.println(fan);
-
-    Serial.print("Light: ");
-    Serial.println(light);
-
-    Serial.println("");
-
-    //switch 1
-      digitalWrite(Light,light);
-
-      digitalWrite(Fan,fan);
-
-    http.end();
     
+    bool temp = doc2["fan"]; 
+    bool light= doc2["light"]; 
+
+    digitalWrite(Fan,temp);
+    digitalWrite(Light,light);
+    
+    // Free resources
+    http.end();
   }
   else {
     Serial.println("WiFi Disconnected");
